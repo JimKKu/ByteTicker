696aa8bae80028f20c13c253ac4165b4a97c14eb
Jim <sharycc@163.com>
2025-04-08 00:26:20 +0800

    打印工具类

---
+++ src/main/java/com/jim/ssr/utils/PrinterUtils.java
@@ -0,0 +1,57 @@
+package com.jim.ssr.utils;
+
+
+/**
+ * @Author: Jim
+ * @Description: 打印工具类
+ * TODO 具体参数后面调整
+ */
+public class PrinterUtils {
+
+
+    /**
+     * 打印标题（大字加粗）
+     * 1. 店名
+     */
+    public static void TITLE1(){}
+
+    /**
+     * 副标题（小字号）
+     * 1. 祝您用餐愉快
+     */
+    public static void TITLE2(){}
+
+    /**
+     * 打印副标题区（小子号）
+     * 1. 下单时间 日期+时间
+     * 2. 单号（10000 ~ 99999）
+     */
+    public static void INFO(){}
+
+    /**
+     * 打印订单流水号（大字加粗）
+     * 1. 订单流水号 0001 ~ 9999
+     */
+    public static void NUM(){}
+
+    /**
+     * 打印菜单(小字号)
+     * 1. 品相 ---- 数量 ---- 价格
+     * 2. 番茄炒鸡蛋 1份 ----  18元
+     * 3. 油焖茄子 - 1份 ----  18元
+     */
+    public static void MENU(){}
+
+    /**
+     * 打印价格（明细-可省略 - 小字号）
+     * 1. 价格
+     */
+    public static void PRICE_DETAIL(){}
+
+    /**
+     * 打印价格（总价 - 大字号）
+     * 应付金额 xx元
+     */
+    public static void PRICE_TOTAL(){}
+
+}

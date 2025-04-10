f3418f1ce2c0882e87f45049995fd2620b47ef51
Jim.win <sharycc@163.com>
2025-04-10 15:37:08 +0800

    无用代码删除

--- src/main/java/com/jim/ssr/SsrApplication.java
+++ src/main/java/com/jim/ssr/SsrApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SsrApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SsrApplication.class, args);
     }
